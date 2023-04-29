c8dbdf3ee90c3c189e0a477b217fc7542236c7fe
Gr4childo <samohkin.2012@gmail.com>
2023-04-29 15:14:35 +0300

    fixes

--- src/ui/components/Contacts/index.jsx
+++ src/ui/components/Contacts/index.jsx
@@ -4,13 +4,13 @@ const Contacts = ({ ...props }) => {
       <span {...props}>+7 555 555-55-55</span>
       <ul {...props}>
         <li>
-          <img src='/telegram.svg' alt='' />
+          <img src='/src/assets/telegram.svg' alt='' />
         </li>
         <li>
-          <img src='/viber.svg' alt='' />
+          <img src='/src/assets/viber.svg' alt='' />
         </li>
         <li>
-          <img src='/whatsapp.svg' alt='' />
+          <img src='/src/assets/whatsapp.svg' alt='' />
         </li>
       </ul>
     </div>
